eed04ecff203c51b8d2bcad3a0197ebf537c6ccc
cedrichermans94 <hermans.cedric@outlook.com>
2026-01-05 23:16:54 +0100

    Added core layout
    Explanation text + simulation panel

--- physics-engine/src/Main.java
+++ physics-engine/src/Main.java
@@ -1,13 +1,42 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    // Setting up JFrame
+    JFrame frame = new JFrame();
+    frame.setSize(500, 300);
+    frame.setTitle("Physics Engine - Mk I");
+    frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    // Setting up main JPanel
+    JPanel mainPanel = new JPanel(new BorderLayout());
+    mainPanel.setPreferredSize(frame.getSize());
+
+    // Setting up label on top
+    JTextPane explanationLabel = new JTextPane();
+    explanationLabel.setText("Click anywhere on the simulation panel to generate a cube that falls in a vacuum at Earth's gravity. The maximum height of the simulation panel is 5 kilometer.");
+    explanationLabel.setPreferredSize(new Dimension(500,50));
+    explanationLabel.setOpaque(true);
+    explanationLabel.setBackground(Color.BLUE);
+    explanationLabel.setForeground(Color.WHITE);
+    mainPanel.add(explanationLabel, BorderLayout.NORTH);
+
+    // Setting up simulation panel
+    JPanel simulationPanel = new JPanel();
+    Border inside = new LineBorder(Color.BLACK, 5);
+    Border outside = new EmptyBorder(20,20,20,20);
+    simulationPanel.setBorder(new CompoundBorder(outside, inside));
+    mainPanel.add(simulationPanel, BorderLayout.CENTER);
+
+    frame.setContentPane(mainPanel);
+
+    //
+    frame.pack();
+    frame.setVisible(true);
 }
