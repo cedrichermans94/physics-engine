14afd8d8a694ee60428c52f311d9cc1766ded422
cedrichermans94 <hermans.cedric@outlook.com>
2026-01-10 17:09:47 +0100

    Added cube class

--- physics-engine/src/SimulationPanel.java
+++ physics-engine/src/SimulationPanel.java
@@ -2,14 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class SimulationPanel extends JPanel {
-    public SimulationPanel() {
+    Cube cube;
 
+    public SimulationPanel() {
+        cube = new Cube(100,100,100,100);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponents(g);
-        g.drawRect(100,100,100,100);
+        g.drawRect(cube.getX(), cube.getY(), cube.getWidth(), cube.getHeight());
         g.setColor(Color.black);
         g.fillRect(100,100,100,100);
     }
