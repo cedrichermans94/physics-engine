143de5319799305dea3c1d9bb400af15b38e17ed
cedrichermans94 <hermans.cedric@outlook.com>
2026-01-27 20:22:13 +0100

    Make executable jar

--- physics-engine/src/Main.java
+++ physics-engine/src/Main.java
@@ -4,81 +4,82 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.util.Objects;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    // Setting up JFrame
-    System.setProperty( "apple.awt.application.name", "Physics Engine - Mk I" );
-    int width = Toolkit.getDefaultToolkit().getScreenSize().width;
-    int height = Toolkit.getDefaultToolkit().getScreenSize().height;
-    int infoHeight = 100;
-    int infoWidth = 100;
-    JFrame frame = new JFrame();
-    frame.setSize(width, height);
-    frame.setMinimumSize(new Dimension(800,400));
-    frame.setTitle("Physics Engine - Mk I");
-    frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    frame.setVisible(true);
+public class Main {
+    public static void main(String[] args) {
+        // Setting up JFrame
+        System.setProperty( "apple.awt.application.name", "Physics Engine - Mk I" );
+        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+        int infoHeight = 100;
+        int infoWidth = 100;
+        JFrame frame = new JFrame();
+        frame.setSize(width, height);
+        frame.setMinimumSize(new Dimension(800,400));
+        frame.setTitle("Physics Engine - Mk I");
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
 
-    // Setting up main panel
-    JPanel mainPanel = new JPanel(new BorderLayout());
-    mainPanel.setPreferredSize(frame.getSize());
+        // Setting up main panel
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setPreferredSize(frame.getSize());
 
-    // Setting up information panel
-    JPanel infoPanel = new JPanel(new BorderLayout());
-    //infoPanel.setPreferredSize(new Dimension(frame.getWidth(), infoHeight));
-    mainPanel.add(infoPanel, BorderLayout.NORTH);
+        // Setting up information panel
+        JPanel infoPanel = new JPanel(new BorderLayout());
+        //infoPanel.setPreferredSize(new Dimension(frame.getWidth(), infoHeight));
+        mainPanel.add(infoPanel, BorderLayout.NORTH);
 
-    // Setting up icon on left
-    ImageIcon icon = new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/img/icon.png")));
-    Image image = icon.getImage();
-    Image scaledImage = image.getScaledInstance(infoWidth, infoHeight, Image.SCALE_SMOOTH);
-    ImageIcon scaledIcon = new ImageIcon(scaledImage);
-    JLabel iconLabel = new JLabel(scaledIcon);
-    iconLabel.setPreferredSize(new Dimension(infoWidth,infoHeight));
-    infoPanel.add(iconLabel, BorderLayout.WEST);
+        // Setting up icon on left
+        ImageIcon icon = new ImageIcon(Objects.requireNonNull(Main.class.getResource("/img/icon.png")));
+        Image image = icon.getImage();
+        Image scaledImage = image.getScaledInstance(infoWidth, infoHeight, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        JLabel iconLabel = new JLabel(scaledIcon);
+        iconLabel.setPreferredSize(new Dimension(infoWidth,infoHeight));
+        infoPanel.add(iconLabel, BorderLayout.WEST);
 
-    // Setting up info label on right
-    JLabel explanationLabel = new JLabel("", SwingConstants.CENTER);
-    explanationLabel.setText("<html>" +
-            "Click anywhere on the simulation panel to generate a cube that falls in a vacuum under Earth's gravity.<br> " +
-            "The maximum height of the simulation panel is 5 kilometers and the cube has a size of 100 meters.");
-    //explanationLabel.setPreferredSize(new Dimension(frame.getWidth() - infoWidth,infoHeight));
-    explanationLabel.setOpaque(true);
-    explanationLabel.setBackground(Color.BLUE);
-    explanationLabel.setForeground(Color.WHITE);
-    infoPanel.add(explanationLabel, BorderLayout.CENTER);
+        // Setting up info label on right
+        JLabel explanationLabel = new JLabel("", SwingConstants.CENTER);
+        explanationLabel.setText("<html>" +
+                "Click anywhere on the simulation panel to generate a cube that falls in a vacuum under Earth's gravity.<br> " +
+                "The maximum height of the simulation panel is 5 kilometers and the cube has a size of 100 meters.");
+        //explanationLabel.setPreferredSize(new Dimension(frame.getWidth() - infoWidth,infoHeight));
+        explanationLabel.setOpaque(true);
+        explanationLabel.setBackground(Color.BLUE);
+        explanationLabel.setForeground(Color.WHITE);
+        infoPanel.add(explanationLabel, BorderLayout.CENTER);
 
-    // Setting up simulation panel
-    SimulationPanel simulationPanel = new SimulationPanel();
-    Border inside = new LineBorder(Color.BLACK, 5);
-    Border outside = new EmptyBorder(20,20,20,20);
-    simulationPanel.setBorder(new CompoundBorder(outside, inside));
-    mainPanel.add(simulationPanel, BorderLayout.CENTER);
-    simulationPanel.addMouseListener(simulationPanel);
+        // Setting up simulation panel
+        SimulationPanel simulationPanel = new SimulationPanel();
+        Border inside = new LineBorder(Color.BLACK, 5);
+        Border outside = new EmptyBorder(20,20,20,20);
+        simulationPanel.setBorder(new CompoundBorder(outside, inside));
+        mainPanel.add(simulationPanel, BorderLayout.CENTER);
+        simulationPanel.addMouseListener(simulationPanel);
 
-    // finalize frame
-    frame.setContentPane(mainPanel);
-    frame.pack();
+        // finalize frame
+        frame.setContentPane(mainPanel);
+        frame.pack();
 
-    // init simulation
-    simulationPanel.init();
+        // init simulation
+        simulationPanel.init();
 
-    // start simulation
-    int ups = 60;
-    simulationPanel.start();
-    long previousTime = System.currentTimeMillis();
-    long wait = 0;
-    while (simulationPanel.isStarted()) {
-        long currentTime = System.currentTimeMillis();
-        long deltaTime = currentTime - previousTime;
-        previousTime = currentTime;
-        wait += deltaTime;
-        while (wait >= 1000/ups) {
-            simulationPanel.update(wait);
-            simulationPanel.repaint();
-            wait = 0;
+        // start simulation
+        int ups = 60;
+        simulationPanel.start();
+        long previousTime = System.currentTimeMillis();
+        long wait = 0;
+        while (simulationPanel.isStarted()) {
+            long currentTime = System.currentTimeMillis();
+            long deltaTime = currentTime - previousTime;
+            previousTime = currentTime;
+            wait += deltaTime;
+            while (wait >= 1000/ups) {
+                simulationPanel.update(wait);
+                simulationPanel.repaint();
+                wait = 0;
+            }
         }
     }
 }
